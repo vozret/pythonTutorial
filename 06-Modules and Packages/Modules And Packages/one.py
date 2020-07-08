eee4544f84a68ca6d6c3558e97ceb1c8baf55756
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-07-08 17:26:01 +0200

    Section 9 and 10, using Sublime Text Editor

---
+++ 06-Modules and Packages/Modules And Packages/one.py
@@ -0,0 +1,18 @@
+#one.py
+
+def func():
+	print('FUNC() IN ONE.PY')
+
+def function():
+	pass
+
+def function2():
+	pass
+
+print('TOP LEVEL IN ONE.PY')
+
+if __name__ == '__main__':
+	# RUN THE SCRIPT!
+	function2()
+	func()
+	function()
